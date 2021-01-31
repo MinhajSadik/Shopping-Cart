f12939ba6186d7ab65a5996f6ac57e2000fa890d
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-02-01 00:03:26 +0600

    final Update

---
+++ script.js
@@ -0,0 +1,53 @@
+// function handlePhoneChange(isIncrease) {
+      //       const phoneInput = document.getElementById('phone-count');
+      //       const phoneCount = parseInt(phoneInput.value);
+      //       let phoneNewCount = phoneCount;
+      //       if (isIncrease == true) {
+      //          phoneNewCount = phoneCount + 1;
+      //       }
+      //       if (isIncrease == false && phoneCount > 0) {
+      //          phoneNewCount = phoneCount - 1;
+
+      //       }
+      //       phoneInput.value = phoneNewCount;
+      //       const phoneTotal = phoneNewCount * 1219;
+      //       document.getElementById('phone-total').innerText = '$' + phoneTotal;
+      //    }
+
+      // function handleCaseChange(isIncrease){
+      //    const caseInput = document.getElementById('case-count');
+      //    const caseCount = parseInt(caseInput.value);
+      //    let caseNewCount = caseCount;
+      //    if(isIncrease == true){
+      //       caseNewCount = caseCount + 1;
+
+      //    }
+      //    if(isIncrease == false && caseCount > 0){
+      //       caseNewCount = caseCount - 1;
+
+      //    }
+      //    caseInput.value = caseNewCount;
+      //    const casseTotal = caseNewCount * 30;
+      //    document.getElementById('case-total').innerText = '$' + casseTotal;
+      // }
+
+                  ////////////Doesn't Working This Code
+      // document.getElementById('case-increase').addEventListener('click', function(){
+      //    let caseInput = document.getElementById("case-count");
+      //    let caseCount = parseInt(caseInput.value);
+      //    let caseNewCount = caseCount + 1;
+      //    caseInput.value = caseNewCount;
+      //    let caseTotal = caseNewCount * 30;
+      //    document.getElementById('case-total').innerText = '$', + caseTotal;
+      // });
+
+      // document.getElementById('case-decrease').addEventListener('click', function(){
+      //    let caseInput = document.getElementById("case-count");
+      //    let caseCount = parseInt(caseInput.value);
+      //    let caseNewCount = caseCount - 1;
+      //    caseInput.value = caseNewCount;
+      //    let caseTotal = caseNewCount * 30;
+      //    document.getElementById('case-total').innerText = '$', + caseTotal;
+      // });
+
+
